6698eb6e1412d8fa52bbcf22a07dc10fef8478e1
Denys-Hryvastov <dhryvastov@opentext.com>
2023-09-06 20:15:35 +0300

    sdsdsd

---
+++ Components/MyFlight/MyFlight-Login-9/Res2/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'D:\\BptTests\\111\\mbt_demo\\Components\\MyFlight\\MyFlight-Login-4\\Res2',namePairs:[]}]};
