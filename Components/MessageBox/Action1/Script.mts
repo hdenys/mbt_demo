ae01c14b1f1ffec52360d1f36d9735d4a4a63ef1
Denys-Hryvastov <dhryvastov@opentext.com>
2023-09-13 10:27:23 +0300

    MsgBox

---
+++ Components/MessageBox/Action1/Script.mts
@@ -0,0 +1,2 @@
+﻿Set WshShell = CreateObject("WScript.Shell")
+	WshShell.Popup "It will be closed in 5 seconds...", 5, "5 Second"
---
+++ Components/MessageBox/Res1/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'D:\\BptTests\\111\\mbt_demo\\Components\\MessageBox\\Res1',namePairs:[]}]};
